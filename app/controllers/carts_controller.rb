de944a16d716d0101d142ec5de1534c3f1f954c8
Keith Bachand <keithbachand14@gmail.com>
2016-05-19 11:56:17 -0400

    added total and subtotal to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,21 +3,22 @@ before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
   # GET /carts
   # GET /carts.json
+
   def index
     @carts = Cart.all
   end
 
   def show
-    #the cart is not secure until we make sure you can only see a cart that matches you as a user
-    #may need to drop your database after this is implemented rake db:reset add products and users
-    # if current_user
-    #   unless current_user.id == @cart.user_id
-    #   flash[:notice] = "You don't have access to that order!"
-    #   redirect_to root_path
-    #   end
-    # else
-    #   redirect_to user_session_path, notice: 'Please sign-in first.'
-    # end
+    # the cart is not secure until we make sure you can only see a cart that matches you as a user
+    # may need to drop your database after this is implemented rake db:reset add products and users
+    if current_user
+      unless current_user.id == @cart.user_id
+      flash[:notice] = "You don't have access to that order!"
+      redirect_to root_path
+      end
+    else
+      redirect_to user_session_path, notice: 'Please sign-in first.'
+    end
   end
 
   # GET /carts/new
@@ -60,7 +61,7 @@ before_action :set_cart, only: [:show, :edit, :update, :destroy]
     #session is a rails method that allows us to store temp data without saving it to a dbase
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Cart was successfully destroyed.' }
     end
   end
 
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :ailment
+
+  def total_price
+    ailment.price * quantity
+  end
 end
