4f66b4f90037e7dd27faabfddd691eab574a32f6
Keith Bachand <keithbachand14@gmail.com>
2016-11-02 12:29:42 -0400

    took out files not needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'csv'
-namespace :import_ailment do
-desc "Import ailment from a CSV file"
-task ailments: :environment do
-CSV.foreach("ailments.csv") do |row|
-    name, description, shopping_list, price = row
-    Ailment.create(name: name, description: description, shopping_list: shopping_list, price: price)
-    end
-  end
-end
